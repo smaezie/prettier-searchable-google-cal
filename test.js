7c55260b2c0b0fc7c7d8393990e120fb5dee8e57
smaezie <sarahmaezareski@gmail.com>
2026-02-10 20:21:12 +0000

    New Javascript functions. Do they call? Let's find out!

---
+++ test.js
@@ -0,0 +1,110 @@
+// what the fuck - API KEYS and IDs (check logs for this info so i dont cause security issues)
+const CLIENT_ID = '368987126201-ke47d1ma0an8pp720dbu1l37kc0hh0nm.apps.googleusercontent.com';
+const API_KEY = '<YOUR_API_KEY>';
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+
+// Authorization scopes required by the API; multiple scopes can be included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+document.getElementById('authorize_button').style.visibility = 'hidden';
+document.getElementById('signout_button').style.visibility = 'hidden';
+
+/**
+* Callback after api.js is loaded.
+*/
+function gapiLoaded() {
+    gapi.load('client', initializeGapiClient)
+}
+
+/**
+* Enables user interaction after all libraries are loaded.
+*/
+function maybeEnableButtons() {
+    if (gapiInited && gisInited) {
+        document.getElementById('authorize_button').style.visibility = 'visible';
+    }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick() {
+tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) {
+    throw (resp);
+    }
+    document.getElementById('signout_button').style.visibility = 'visible';
+    document.getElementById('authorize_button').innerText = 'Refresh';
+    await listUpcomingEvents();
+};
+
+if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    tokenClient.requestAccessToken({prompt: 'consent'});
+} 
+else {
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({prompt: ''});
+    }
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick() {
+const token = gapi.client.getToken();
+if (token !== null) {
+    google.accounts.oauth2.revoke(token.access_token);
+    gapi.client.setToken('');
+    document.getElementById('content').innerText = '';
+    document.getElementById('authorize_button').innerText = 'Authorize';
+    document.getElementById('signout_button').style.visibility = 'hidden';
+    }
+}
+
+/**
+ * Print the summary and start datetime/date of the next ten events in
+ * the authorized user's calendar. If no events are found an
+ * appropriate message is printed.
+ */
+async function listUpcomingEvents() {
+    let response;
+    try {
+        const request = {
+        'calendarId': 'primary',
+        'timeMin': (new Date()).toISOString(),
+        'showDeleted': false,
+        'singleEvents': true,
+        'maxResults': 10,
+        'orderBy': 'startTime',
+        };
+        response = await gapi.client.calendar.events.list(request);
+    } 
+    catch (err) {
+        document.getElementById('content').innerText = err.message;
+        return;
+    }
+
+    const events = response.result.items;
+    if (!events || events.length == 0) {
+        document.getElementById('content').innerText = 'No events found.';
+        return;
+    }
+    // Flatten to string to display
+    const output = events.reduce(
+        (str, event) => `${str}${event.summary} (${event.start.dateTime || event.start.date})\n`,
+        'Events:\n');
+    document.getElementById('content').innerText = output;
+}
+
+//testing functions
+function TurnHeaderBlue() {
+    document.getElementById("Header").style.color = "blue";
+}
