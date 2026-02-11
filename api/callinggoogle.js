1406bed6cf692843e8576c40d7941cb1f8710163
smaezie <sarahmaezareski@gmail.com>
2026-02-11 19:21:50 +0000

    adjustment to the timeMax formula for dynamic months (feb and leap)

--- api/callinggoogle.js
+++ api/callinggoogle.js
@@ -1,5 +1,7 @@
 import { google } from "googleapis";
 
+selectMonth = document.getElementById("month");
+selectYear = document.getElementById("year");
 
 export default async function handler(request, response) {
 
@@ -8,11 +10,11 @@ export default async function handler(request, response) {
         auth: process.env.GOOGLE_CAL_API_KEY, // API key only works for PUBLIC calendars
     });
 
-    //when i get my promise bach .THEN(i will do this)
+    //when i get my promise back .THEN(i will do this)
     const res = await calendar.events.list({
         calendarId: "c_6c371d91700f944faa41f0885a53ef93e08b9eb4519ea076f2981edddb92f659@group.calendar.google.com",
         timeMin: new Date().toISOString(),
-        timeMax: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString(),
+        timeMax: new Date(new Date(parseInt(selectYear.value), parseInt(selectMonth.value) + 1, 0, 23, 59, 59, 999).toISOString()),
         singleEvents: true,
         orderBy: "startTime",
     });
--- calendarview.js
+++ calendarview.js
@@ -4,7 +4,7 @@ currentYear = today.getFullYear();
 selectYear = document.getElementById("year");
 selectMonth = document.getElementById("month");
 
-months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 monthAndYear = document.getElementById("monthAndYear");
 showCalendar(currentMonth, currentYear);
